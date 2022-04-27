5284e6b5f7b98b4f4e67a845415b44c4cbc5f49e
leandrosconceicao <leandrosantosconceicao@gmail.com>
2022-04-27 14:17:35 -0300

    feat: criando a primeira rota post

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,3 +1,9 @@
 module.exports = app => {
-    app.get('/', (req, res) => res.send('Servidor rodando, tudo ok.'))
+    app.get('/atendimentos', (req, res) => res.send('Servidor rodando, tudo ok.'))
+
+    app.post('/atendimentos', (req, res) => 
+        {
+            console.log(req.body)
+            res.send('Você está na rota de atendimentos e está realizando um POST')
+        })
 }
